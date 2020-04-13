5358cf39bd5d83752c97da03918ed109b53c74d6
ltreven <ltreven@gmail.com>
2020-04-13 14:21:44 +0200

    finish exercise

--- code/app/src/main/java/com/example/dataadapters/MainActivity.java
+++ code/app/src/main/java/com/example/dataadapters/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dataadapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] names = new String[10];
+        names[0] = "Lourenço";
+        names[1] = "Kanya";
+        names[2] = "Peter";
+        names[3] = "Nadine";
+        names[4] = "Nano";
+        names[5] = "Rodrigo";
+        names[6] = "Danilo";
+        names[7] = "Rainer";
+        names[8] = "Paul";
+        names[9] = "Felipe";
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        itemsAdapter.addAll(names);
+        ListView listView = (ListView) findViewById(R.id.students);
+
+        listView.setAdapter(itemsAdapter);
+
+
     }
 }
